a946301cd90839d6ec948980d0601ba9ffaacd3b
Yusuf Hosny <yusufyy2004@hotmail.com>
2025-05-29 22:24:12 +0200

    initial commit

---
+++ src/test/java/com/oilerrig/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oilerrig.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
